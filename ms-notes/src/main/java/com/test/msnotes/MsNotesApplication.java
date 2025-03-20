6240e81db3795d74299e0e12631b49320e998c84
Esteban <150131639+Esteban-Bare@users.noreply.github.com>
2025-03-20 15:32:44 +0100

    Add Maven wrapper and initial Spring Boot application setup for diabetes and notes services

---
+++ ms-notes/src/main/java/com/test/msnotes/MsNotesApplication.java
@@ -0,0 +1,15 @@
+package com.test.msnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MsNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsNotesApplication.class, args);
+    }
+
+}
